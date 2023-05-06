0b3839864c4d6dda559893aeb9bddda629ff3d2a
redgradient <neutral1shade@yahoo.com>
2023-05-06 16:56:07 +0400

    fix checkstyle errors

--- src/main/java/com/redgradient/urlshrinker/model/UrlPair.java
+++ src/main/java/com/redgradient/urlshrinker/model/UrlPair.java
@@ -1,6 +1,10 @@
 package com.redgradient.urlshrinker.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
